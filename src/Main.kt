f1354f66f0b30a5d6333917321b9917a3e8d01d7
Stevie-hue <infoinnorik@gmail.com>
2026-05-06 11:48:36 +0000

    user input

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 fun main(vararg  args: String){
     if (args.isEmpty()) {
     print("Please pass a argument to the command line or pass an args to the e" +
@@ -6,4 +8,13 @@ fun main(vararg  args: String){
     println("the first arg is ${args[0]}");
 
     for (arg in args) println(arg)
+
+    // user input
+    print("enter some file:")
+    val filename = readLine() ?: ""
+    print("this is your file ${filename}")
+
+    var isValidFile = File(filename).isFile
+    if (isValidFile)  true;
+    else false;
 }

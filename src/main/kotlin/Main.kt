3880c4390c83453e4e983bb4d2c56a6aec92f2eb
Stevie-hue <infoinnorik@gmail.com>
2026-05-07 15:54:32 +0000

    interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
+import kotlin.reflect.typeOf
+
+interface StringProvider {
+    fun  getString(num:Int): String
+
+    fun placeHolder(id:Int): Long = id.toLong() // this works because kotlin we an initialize our prop method
+
+}
+class DefaultResourceProvider : StringProvider{
+    override fun getString(num: Int): String {
+        return ("id $num")
+    }
+    fun checkingType(){
+        println("type of" + typeOf<String>())
+    }
+
+}
 fun main(){
-val person = Person(0)
+    var stringProvider:StringProvider = DefaultResourceProvider();
+    print(stringProvider.getString(10))
+    var df  = DefaultResourceProvider()
+    df.checkingType()
+
+
 }

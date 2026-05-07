b36eb9745c57fef25a4ae86395bc69be36e245b8
Stevie-hue <infoinnorik@gmail.com>
2026-05-07 16:48:02 +0000

    data classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,4 +15,13 @@ val directions = Directions.EAST
 val color = HighlightColor.BLUES
     print(color.name)
 //    print(color.values)
-}
+
+    // Data classes
+    val data1 = Task("1","Feli")
+    val data2  = Task("2","Nora")
+    val data2copy = data2.copy()
+    val (id,name) = data1;
+
+    if(data1.name == data2.name) println("same")
+    else println("not same")
+ }
